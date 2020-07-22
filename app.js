638c3196dcde6726c8a9888477d314e1dc8b8dd9
imjchiang <jchiang296@gmail.com>
2020-07-22 16:51:55 -0700

    [JS] pokemon card complete

--- app.js
+++ app.js
@@ -43,24 +43,46 @@ fetch("https://pokeapi.co/api/v2/pokemon/lickitung")
     spriteImg.style.width = "220px";
     spriteImg.style.height = "175px";
     spriteImg.style.objectFit = "cover";
-
     spriteImg.style.border = "6px solid yellow";
     spriteImg.style.margin = "15px";
     spriteImg.style.marginTop = "0px";
+    spriteImg.style.marginBottom = "0px";
     spriteImg.style.backgroundColor = "mediumPurple";
     spriteImg.setAttribute("src", frontDefault);
     
     card.appendChild(spriteImg);
 
-    
+    let spriteInfo = document.createElement("p");
+    spriteInfo.classList.add("card-img-top");
+    spriteInfo.style.width = "220px";
+    spriteInfo.style.border = "6px solid yellow";
+    spriteInfo.style.marginTop = "0px";
+    spriteInfo.style.marginLeft = "15px";
+    spriteInfo.style.position = "relative";
+    spriteInfo.style.top = "-6px";
+    spriteInfo.textContent = "NO. 108  Licking Pokemon  HT: " + height + "  WT: " + weight;
+    spriteInfo.style.fontSize = "10px";
+
+    card.appendChild(spriteInfo);
+
     let cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
+    cardBody.style.paddingTop = "0";
     card.appendChild(cardBody);
 
+    let content0 = document.createElement("p");
+    content0.classList.add("card-text");
+    content0.textContent = lick.toUpperCase().slice(0, 1) + lick.slice(1, 9);
+    cardBody.appendChild(content0);
 
-    let content = document.createElement("p");
-    content.classList.add("card-text");
-    content.textContent = lick;
-    cardBody.appendChild(content);
+    let content1 = document.createElement("p");
+    content1.classList.add("card-text");
+    content1.textContent = "Flip a coin. If heads, your opponent's Active Pokemon is now Paralyzed.";
+    content1.style.fontSize = "15px";
+    //HERE
 
+    let content2 = document.createElement("p");
+    content2.classList.add("card-text");
+    content2.textContent = "Type: " + type.toUpperCase().slice(0, 1) + type.slice(1, 9);
+    cardBody.appendChild(content2);
 })
